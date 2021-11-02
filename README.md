d676989aa54025c886093c397331b314d3f3f415
olevole <olevole@olevole.ru>
2021-11-03 01:40:36 +0300

    Import

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # modules-forms-mysql
-CBSD module for manage MySQL server
+CBSD module to manage mysql
+
+Info: this module uses the CBSD puppet module
+
+To install:
+
+  - cbsd module mode=install forms-mysql
+
---
+++ initforms.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+pgm="${0##*/}"		# Program basename
+progdir="${0%/*}"	# Program directory
+: ${REALPATH_CMD=$( which realpath )}
+: ${SQLITE3_CMD=$( which sqlite3 )}
+: ${RM_CMD=$( which rm )}
+: ${MKDIR_CMD=$( which mkdir )}
+: ${FORM_PATH="/opt/forms"}
+: ${distdir="/usr/local/cbsd"}
+
+MY_PATH="$( ${REALPATH_CMD} ${progdir} )"
+HELPER="mysql"
+
+# MAIN
+if [ -z "${workdir}" ]; then
+	[ -z "${cbsd_workdir}" ] && . /etc/rc.conf
+	[ -z "${cbsd_workdir}" ] && exit 0
+	workdir="${cbsd_workdir}"
+fi
+
+set -e
+. ${distdir}/cbsd.conf
+. ${subrdir}/tools.subr
+. ${subr}
+set +e
+
+FORM_PATH="${workdir}/formfile"
+
+[ ! -d "${FORM_PATH}" ] && err 1 "No such ${FORM_PATH}"
+[ -f "${FORM_PATH}/${HELPER}.sqlite" ] && ${RM_CMD} -f "${FORM_PATH}/${HELPER}.sqlite"
+
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms.schema forms
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms.schema additional_cfg
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms_system.schema system
+
+# yesno
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms_yesno.schema skip_networking_yesno
+
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,1,"-Globals","Globals",'Globals','PP','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,2,"mysql_ver","mysql version",'80','80','',1, "maxlen=5", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,3,"-Additional","Additional params",'Additional params','','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,4,"port","port",'3306','3306','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,5,"socket","socket",'/tmp/mysql.sock','/tmp/mysql.sock','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"bind_address","bind_address",'127.0.0.1','127.0.0.1','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,7,"skip_networking","skip_networking",'false','false','',1, "maxlen=60", "radio", "skip_networking_yesno", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,8,"expire_logs_days","expire_logs_days",'10','10','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,9,"key_buffer_size","key_buffer_size",'16M','16M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,10,"max_allowed_packet","max_allowed_packet",'16M','16M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,11,"max_binlog_size","max_binlog_size",'100M','100M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,12,"max_connections","max_connections",'151','151','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,13,"thread_cache_size","thread_cache_size",'8','8','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,14,"thread_stack","thread_stack",'256K','256K','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,16,"sort_buffer_size","sort_buffer_size",'8M','8M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,200,"-Databases","Databases",'Databases','-','',1, "maxlen=60", "delimer", "", "dbgroup" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,201,"databases","Add databases",'201','','',0, "maxlen=60", "group_add", "", "dbgroup" );
+#INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,300,"-HBARules","HBA Rules",'HBA Rules','-','',1, "maxlen=60", "delimer", "", "hbagroup" );
+#INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,301,"hba_rules","HBA Rules",'301','','',0, "maxlen=60", "group_add", "", "hbagroup" );
+COMMIT;
+EOF
+
+# Put boolean for use_sasl_yesno
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO skip_networking_yesno ( text, order_id ) VALUES ( "true", 1 );
+INSERT INTO skip_networking_yesno ( text, order_id ) VALUES ( "false", 0 );
+COMMIT;
+EOF
+
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "mysql", "201607", "databases/mysql13-server", "mysql-server" );
+COMMIT;
+EOF
+
+# CREATE VIEW
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+CREATE VIEW FORM_VIEW AS SELECT * FROM forms UNION SELECT * FROM additional_cfg;
+COMMIT;
+EOF
+
+# long description
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+UPDATE system SET longdesc='\
+mysql is a multithreaded SQL database. \
+';
+COMMIT;
+EOF
